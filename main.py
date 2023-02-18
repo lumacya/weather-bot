80e6d0a3409f8b564844a287cfe1864e31cce4cd
Ivan Gibert <vngbrt@gmail.com>
2023-02-18 17:15:11 +0300

    make get_weather function

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from config import open_weather_token
+
+def get_weather(city, open_weather_token):
+    try:
+        r = requests.get(
+            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}'
+        )
+        data = r.json()
+        print(data)
+    except Exception as ex:
+        print(ex)
+        print('Check your city name')
+
+def main():
+    city = input('Enter your city: ')
+    get_weather(city, open_weather_token)
+
+if __name__ == "__main__":
+    main()
