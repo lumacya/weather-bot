fc54f1a207ad4aa1ca550ca156ad7e3f8fded3a4
Ivan Gibert <vngbrt@gmail.com>
2023-02-18 17:29:16 +0300

    edited main.py waiting for api key

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
+from pprint import pprint
 from config import open_weather_token
 
 def get_weather(city, open_weather_token):
     try:
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}'
+            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
         )
         data = r.json()
-        print(data)
+        # pprint(data)  # FIX: Drop with error, because waiting for API key will be activate
+
+        city_name = data["name"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+
+        print(f"The weather in {city_name}\nTemperature: {temperature}\nHumidity: {humidity}\nWind Speed: {wind_speed}")
+
     except Exception as ex:
         print(ex)
         print('Check your city name')
